5c991e1dd8399d52c9a38b637b9bdebe71b743f1
pratyaksh1001 <pratyakshkarmahe@gmail.com>
2024-01-28 16:08:15 +0530

    nothing

--- c++/merge sort.cpp
+++ c++/merge sort.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
 using namespace std;
 
-int seperate(int l,int h,int arr[],int n){
-    int m=(l+h)/2;
-    
-}
-
 int main(){
-    int n=20;
-    int arr[20]={4,5,7,89,100,6,4,3,3,5,6,8,0,-20,6,4,2,2,34,26};
 
 }
