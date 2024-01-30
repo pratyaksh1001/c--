201ec86cbd2a49ed17b0314e82c0ccb90b4977b9
pratyaksh1001 <pratyakshkarmahe@gmail.com>
2024-01-30 10:40:08 +0530

    nothing

--- c++/quick sort.cpp
+++ c++/quick sort.cpp
@@ -1,34 +1,49 @@
 #include <iostream>
+#include <chrono>
 using namespace std;
+auto start = chrono::steady_clock::now();
 
-int quick(int arr[],int n,int s,int e,int p){
-    int i=s;
-    int j=e;
-    if(e==s){
+int quick(int arr[],int n,int x,int y,int p){
+    if(y-x==0){
         return 0;
     }
-    while(i>=j){
-        if(arr[j]<arr[p]){
-            int t=arr[j];
-            arr[j]=arr[i];
-            arr[i]=t;
+    int i=x;
+    int j=y;
+    while(true){
+        if(arr[i]<arr[p]){
             i++;
         }
-        if(arr[i]>arr[p]){
-            int t2=arr[j];
-            arr[j]=arr[i];
-            arr[j]=t2;
-            j--;
+        else{   
+            if(arr[j]>arr[p]){
+                j--;
+            }
+            else{
+                int t=arr[i];
+                arr[i]=arr[j];
+                arr[j]=t;
+            }
         }
+        if(i>j){
+        int t2=arr[j];
+        arr[j]=arr[p];
+        arr[p]=t2;
+        quick(arr,n,x+1,j-1,x);
+        quick(arr,n,j+2,y,j+1);
+        break;
     }
-    quick(arr,n,s,p-1,s);
-    quick(arr,n,p+1,e,p+1);
+    }
 }
+
 int main(){
-    int arr[20]={1,2,8,9,5,6,3,4,8,90,34,12,45,67,89,45,13,63,91,80};
     int n=20;
-    quick(arr,n,0,n-1,0);
-    for(int f=0;f<n;f++){
-        cout<<arr[f]<<" ";
+    int arr[20]={4,3,6,7,8,9,5,4,1,2,7,65,34,12,89,65,345,12,44,50};
+    quick(arr,n,1,n-1,0);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
     }
+cout << "\n";
+auto end = chrono::steady_clock::now();
+auto diff = end - start;
+cout << chrono::duration<double, milli>(diff).count() << " ms" << endl;
+return 0;
 }
