4cbb10e45812718abca95673598d91cfb4120af1
pratyaksh1001 <pratyakshkarmahe@gmail.com>
2024-03-15 11:02:24 +0530

    nothing

--- c/practice12.c
+++ c/practice12.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
-int main
+#include <math.h>
+
+int fact(int n){
+     int r=1;
+     for(int i=1;i<=n;i++){
+          r*=i;
+     }
+     return r;
+}
+void main(){
+     int n=10;
+     int r=0;
+     for(int i=1;i<=n;i++){
+          r=r+(fact(i)/pow(i,i));
+     }
+     printf("result is: %d\n",r);
+}
