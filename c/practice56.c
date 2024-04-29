8c0bf55bb6ea8aaa8de7b1ea3f3a5c78a849246f
pratyaksh1001 <pratyakshkarmahe@gmail.com>
2024-04-29 11:24:52 +0530

    nothing

---
+++ c/practice56.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//count the digits using recursion
+int count(int n,int c){
+    if(n<10){
+        return c;
+    }
+    else{
+        c++;
+        n/=10;
+        count(n,c);
+    }
+}
+int main(){
+    int n=15;
+    int c=1;
+    printf("%d\n",count(n,c));
+},
